8d356833afdc9a08a142616f3714555045081947
takayuki <taka4852@i.softbank.jp>
2017-06-24 01:37:41 +0000

    link_create

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -7,9 +7,9 @@
 @if(count($tasks)>0)
     <ul>
         @foreach($tasks as $task)
-        <li>{!! link_to_route('messages.show', $message->id, ['id' => $message->id]) !!} : {{ $message->content }}</li>
+        <li>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!} : {{ $task->content }}</li>
         @endforeach
     </ul>
 @endif
-
+{!! link_to_route('tasks.create','新規メッセージの投稿') !!}
 @endsection
